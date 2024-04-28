1ea29f5775c9432d11e22456d636dd516dab956f
Cathret <cathret@users.noreply.github.com>
2024-04-28 18:30:33 +0200

    Added refresh config on Home button for aerial fast fall

--- AerialFastFallMod/Patches/AerialFastFall.cs
+++ AerialFastFallMod/Patches/AerialFastFall.cs
@@ -10,6 +10,8 @@ namespace BetInExTesting.Patches;
 [HarmonyPatch(typeof(PixelKartPhysics), "FixedUpdate")]
 public class AerialFastFall
 {
+    private static AerialFastFallPlugin ms_plugin = null;
+    
     private static ConfigEntry<float> ConfigFastFallRate { get; set; }
     private static ConfigEntry<float> ConfigMinimumJoystickInputBeforeFastFall { get; set; }
     private static ConfigEntry<float> ConfigMinimumJumpTimeBeforeFastFall { get; set; }
@@ -18,39 +20,53 @@ public class AerialFastFall
     private static float ms_minimumJoystickInputBeforeFastFall = 0.1f;
     private static float ms_minimumJumpTimeBeforeFastFall = 0.5f;
 
+    private static bool ms_justReloaded = false;
+
     private static void LogDebug(string _text) => AerialFastFallPlugin.Log.LogDebug($"[AerialFastFall]: {_text}");
     private static void LogInfo(string _text) => AerialFastFallPlugin.Log.LogInfo($"[AerialFastFall]: {_text}");
     private static void LogMessage(string _text) => AerialFastFallPlugin.Log.LogMessage($"[AerialFastFall]: {_text}");
+    private static void LogError(string _text) => AerialFastFallPlugin.Log.LogError($"[AerialFastFall]: {_text}");
 
-    public static void Patch(BasePlugin _plugin, Harmony _harmony)
+    public static void Patch(AerialFastFallPlugin _plugin, Harmony _harmony)
     {
-        BindConfigs(_plugin);
+        if (_plugin == null)
+        {
+            LogError("Plugin not found while patching, AerialFastFall will not be loaded");
+            return;
+        }
+        
+        ms_plugin = _plugin;
+        
+        ms_plugin.Config.ConfigReloaded += (_sender, _args) => BindConfigs();
+        
+        BindConfigs();
         
         _harmony.PatchAll(typeof(AerialFastFall));
     }
 
-    private static void BindConfigs(BasePlugin _plugin)
+    private static void ReloadConfigs()
     {
-        ConfigFastFallRate = _plugin.Config.Bind("AerialFastFall", "FastFallRate", 200f, new ConfigDescription("Fast Fall Rate. The higher, the faster the kart will be directed to the ground.", new AcceptableValueRange<float>(0f, 1000f)));
-        ConfigMinimumJoystickInputBeforeFastFall = _plugin.Config.Bind("AerialFastFall", "MinimumJoystickInputBeforeFastFall", 0.1f, new ConfigDescription("Minimum Joystick Input Before Fast Fall in Seconds. Fast Fall Deadzone. While the joystick input is lower than this value, the Fast Fall will not be triggered.", new AcceptableValueRange<float>(0f, 1f)));
-        ConfigMinimumJumpTimeBeforeFastFall = _plugin.Config.Bind("AerialFastFall", "MinimumJumpTimeBeforeFastFall", 0.5f, new ConfigDescription("Minimum Jump Time Before Fast Fall in Seconds. Minimum time after being airborne before Fast Fall is allowed.", new AcceptableValueRange<float>(0f, 5f)));
+        ms_plugin.Config.Reload();
+    }
+
+    private static void BindConfigs()
+    {
+        ConfigFastFallRate = ms_plugin.Config.Bind("AerialFastFall", "FastFallRate", 200f, new ConfigDescription("Fast Fall Rate. The higher, the faster the kart will be directed to the ground.", new AcceptableValueRange<float>(0f, 1000f)));
+        ConfigMinimumJoystickInputBeforeFastFall = ms_plugin.Config.Bind("AerialFastFall", "MinimumJoystickInputBeforeFastFall", 0.1f, new ConfigDescription("Minimum Joystick Input Before Fast Fall in Seconds. Fast Fall Deadzone. While the joystick input is lower than this value, the Fast Fall will not be triggered.", new AcceptableValueRange<float>(0f, 1f)));
+        ConfigMinimumJumpTimeBeforeFastFall = ms_plugin.Config.Bind("AerialFastFall", "MinimumJumpTimeBeforeFastFall", 0.5f, new ConfigDescription("Minimum Jump Time Before Fast Fall in Seconds. Minimum time after being airborne before Fast Fall is allowed.", new AcceptableValueRange<float>(0f, 5f)));
+        
+        ms_fastFallRate = ConfigFastFallRate.Value;
+        ms_minimumJoystickInputBeforeFastFall = ConfigMinimumJoystickInputBeforeFastFall.Value;
+        ms_minimumJumpTimeBeforeFastFall = ConfigMinimumJumpTimeBeforeFastFall.Value;
+
+        LogInfo($"Loaded FastFallRate value [{ms_fastFallRate}]");
+        LogInfo($"Loaded MinimumJoystickInputBeforeFastFall value [{ms_minimumJoystickInputBeforeFastFall}]");
+        LogInfo($"Loaded MinimumJumpTimeBeforeFastFall value [{ms_minimumJumpTimeBeforeFastFall}]");
     }
 
     public static void Prepare(MethodBase original)
     {
         LogMessage(original == null ? "self loading" : "patch loading");
-
-        if (original == null)
-        {
-            ms_fastFallRate = ConfigFastFallRate.Value;
-            LogInfo($"Loaded FastFallRate value [{ms_fastFallRate}]");
-            
-            ms_minimumJoystickInputBeforeFastFall = ConfigMinimumJoystickInputBeforeFastFall.Value;
-            LogInfo($"Loaded MinimumJoystickInputBeforeFastFall value [{ms_minimumJoystickInputBeforeFastFall}]");
-
-            ms_minimumJumpTimeBeforeFastFall = ConfigMinimumJumpTimeBeforeFastFall.Value;
-            LogInfo($"Loaded MinimumJumpTimeBeforeFastFall value [{ms_minimumJumpTimeBeforeFastFall}]");
-        }
     }
 
     private static bool IsAllowedToFastFall(PixelKartPhysics _kartPhysics)
@@ -70,6 +86,9 @@ public class AerialFastFall
     
     public static void Postfix(PixelKartPhysics __instance)
     {
+        if (Input.GetKeyDown(KeyCode.Home))
+            ReloadConfigs();
+        
         if (!IsAllowedToFastFall(__instance))
             return;
         
