54084729985043f60cc5b50d874fae69c2b3f62d
Cathret <cathret@users.noreply.github.com>
2024-05-27 13:51:43 +0200

    Created Twtich Basic Interaction first draft. Reacts properly but modifiers don't work.

---
+++ TwitchBasicInteractions/Patches/TwitchBasicInteractions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using TheKartersModdingAssistant;
+using TheKartersModdingAssistant.Event;
+
+namespace TheKarters2Mods.Patches;
+
+public class TwitchBasicInteractions
+{
+    public void Init()
+    {
+        TwitchIntegrationSDK.OnTwitchChatMessage += OnTwitchChatMessage;
+    }
+
+    public void Destroy()
+    {
+        TwitchIntegrationSDK.OnTwitchChatMessage -= OnTwitchChatMessage;
+    }
+
+    private void OnTwitchChatMessage(string _user, string _message)
+    {
+        if (_user.Equals("cathret") && _message.Equals("death"))
+        {
+            List<Player> allPlayers = Player.GetPlayers();
+            foreach (Player onePlayer in allPlayers)
+            {
+                if (onePlayer.IsHuman())
+                {
+                    onePlayer.SetCurrentHealth(0);
+                }
+            }
+        }
+        
+        if (_user.Equals("cathret") && _message.Equals("boost"))
+        {
+            List<Player> allPlayers = Player.GetPlayers();
+            foreach (Player onePlayer in allPlayers)
+            {
+                if (onePlayer.IsHuman())
+                {
+                    onePlayer.SetCurrentBoostNumber(200);
+                }
+            }
+        }
+    }
+}
