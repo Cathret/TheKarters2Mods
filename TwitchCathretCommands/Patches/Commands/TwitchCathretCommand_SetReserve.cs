b9ca6cc8f9b2d213b923eeca8149249ea77de29c
Cathret <cathret@users.noreply.github.com>
2024-05-28 00:56:50 +0200

    Create TwitchCathretCommands first draft, with only kill and set reserve.

---
+++ TwitchCathretCommands/Patches/Commands/TwitchCathretCommand_SetReserve.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TheKartersModdingAssistant;
+
+namespace TheKarters2Mods.Patches.Commands;
+
+public class TwitchCathretCommand_SetReserve : ITwitchCommand
+{
+    public bool IsActivated()
+    {
+        return TwitchCathretCommands.Instance.ConfigSetReserveCommand.Value;
+    }
+
+    public bool ShouldExecuteCommand(string _user, string[] _command)
+    {
+        if (_command.Length < 4)
+            return false;
+        
+        return _command[1].Equals("reserve") && _command[2].Equals("set");
+    }
+
+    public bool ExecuteCommand(string _user, string[] _command)
+    {
+        int reserve = int.Parse(_command[3]);
+        
+        Player.FindMainPlayer().SetCurrentReserveInPercentage(reserve);
+
+        return true;
+    }
+
+    public string CommandFeedback(string _user, string[] _command)
+    {
+        return $"Thanks {_user}, let them become SPEED";
+    }
+}
